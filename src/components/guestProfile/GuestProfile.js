8c7247a41edfd32cbcc54a90bf7dd23a0b13da5c
Nynke van Schuppen <I410078@fhict.nl>
2021-01-11 09:51:05 +0100

    guestprofile-fix-typo

--- src/components/guestProfile/GuestProfile.js
+++ src/components/guestProfile/GuestProfile.js
@@ -108,7 +108,7 @@ const styles = theme => ({
     boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     padding: "15px"
   }
-});
+);
 
 const languages = Object([
   { code: "NL", language: "Dutch" },
